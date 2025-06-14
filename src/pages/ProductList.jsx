3fd2785fd126d3b41f20e12e900708e6ff4e6b55
Sharmaine Perea <sharmaine.perea23@gmail.com>
2025-06-14 16:19:45 -0600

    implement update favorited product and delete product functionality

--- src/pages/ProductList.jsx
+++ src/pages/ProductList.jsx
@@ -4,12 +4,25 @@ import ProductCard from "./ProductCard";
 
 export default function ProductList() {
 	const {filteredProducts} = useOutletContext();
+	
+  //Delete (Buy Now)
+  const handleDeleteProduct = deletedProductId => setProducts(previousProducts => previousProducts.filter(product => product.id !== deletedProductId))
 
+  //Update Favorited
+  function handleUpdateProduct(updatedProduct) {
+    const updatedProducts = products.map((product) =>
+      product.id === updatedProduct.id ? updatedProduct : product
+    );
+    setProducts(updatedProducts);
+  }
 
 	return (
 		<ul className="cards">
 			{filteredProducts.map((product) => (
-				<ProductCard key={product.id} product={product} />
+				<ProductCard key={product.id} 
+					product={product}
+					onDeleteProduct={handleDeleteProduct}
+					onUpdateProduct={handleUpdateProduct} />
 			))}
 		</ul>
 	)
