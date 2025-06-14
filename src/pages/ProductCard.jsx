a984a90065dc022528575cb96d0189e7171bfbeb
Sharmaine Perea <sharmaine.perea23@gmail.com>
2025-06-14 09:57:24 -0600

    added display products on shop page

--- src/pages/ProductCard.jsx
+++ src/pages/ProductCard.jsx
@@ -1,9 +1,23 @@
-import React from "react";
 
-export default function ProductCard() {
+import React from "react";
+import { useOutletContext, useParams} from "react-router-dom";
+
+export default function ProductCard({product}) {
+	// const { id } = useParams()
+	// const { products } = useOutletContext()
+
+	// const product = products.find((p) => p.id === parseInt(id))
+
+	// if (!product) {
+	// 	return <h2>Product not found.</h2>
+	// }
+
 	return (
-		<div>
-			Product Card!
-		</div>
+		<li className="card" data-testid="product-item">
+			<img src={product.image} width={"50px"} alt={product.name} />
+			<h4>{product.name}</h4>
+			<ul>Price: ${product.price}</ul>
+			<p>{product.description}</p>
+		</li>
 	)
 }
--- src/pages/ProductContainer.jsx
+++ src/pages/ProductContainer.jsx
@@ -3,22 +3,23 @@ import NavBar from "../components/NavBar"
 import {useOutletContext, Link, useParams, Outlet} from "react-router-dom"
 
 export default function ProductContainer() {
-    const [products, setProducts] = useState([])
+    const [products, setProducts] = useState([]);
 
     useEffect(() => {
         fetch("http://localhost:3000/products")
             .then(r => {
-                if (!r.ok) { throw new Error("failed to fetch products") }
+                if (!r.ok) { throw new Error("failed to get products") }
                 return r.json()
             })
             .then(setProducts)
             .catch(console.log)
     }, [])
+
 	return (
 		  <>
             <NavBar />
             <main>
-                <h1>View Products!</h1>
+                <h1>Dancewear</h1>
                 <Outlet context={{products}}/>
             </main>
         </>
